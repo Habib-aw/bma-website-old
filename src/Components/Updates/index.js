f6859d209a0295236533910556ec5183ec106226
Habib-aw <habib.awahid01@gmail.com>
2022-09-04 11:52:44 +0100

    slide right fix

--- src/Components/Updates/index.js
+++ src/Components/Updates/index.js
@@ -1,57 +1,65 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useState,useRef } from 'react';
-import './style.css'
-import eid_adha_2022 from "../../Assets/Home/Updates/eid-adha-2022.jpg"
-import sacrifice_hijjah from "../../Assets/Home/Updates/sacrifice-hijjah.jpeg"
-import ramadan_reflection from "../../Assets/Home/Updates/ramadan-reflection.jpg"
-import takbeer_tashreeq from "../../Assets/Home/Updates/takbeer-tashreeq.jpeg"
-import bengali_dates from "../../Assets/Home/Updates/bengali-dates-water.jpeg"
-import arafat from "../../Assets/Home/Updates/arafat.jpeg"
-import eid_fitr from "../../Assets/Home/Updates/eid-fitr.jpg"
+import React from "react";
+import { useEffect } from "react";
+import { useState, useRef } from "react";
+import "./style.css";
+import eid_adha_2022 from "../../Assets/Home/Updates/eid-adha-2022.jpg";
+import sacrifice_hijjah from "../../Assets/Home/Updates/sacrifice-hijjah.jpeg";
+import ramadan_reflection from "../../Assets/Home/Updates/ramadan-reflection.jpg";
+import takbeer_tashreeq from "../../Assets/Home/Updates/takbeer-tashreeq.jpeg";
+import bengali_dates from "../../Assets/Home/Updates/bengali-dates-water.jpeg";
+import arafat from "../../Assets/Home/Updates/arafat.jpeg";
+import eid_fitr from "../../Assets/Home/Updates/eid-fitr.jpg";
 function Updates() {
-    const [mobileSlider,setMobileSlider]= useState(false);
-    const [slideRight,setSlideRight]= useState(true);
-    useEffect(()=>{
-        if(window.innerWidth<600){
-            setMobileSlider(true)
-        }else{
-            setMobileSlider(false)
-        }
-        window.addEventListener("resize",()=>{
-            if(window.innerWidth<600){
-                setMobileSlider(true)
-            }else{
-                setMobileSlider(false)
-            }
-        })
-    })
-    const images = [
-        eid_adha_2022,
-        arafat,
-        takbeer_tashreeq,
-        bengali_dates,
-        sacrifice_hijjah,
-        ramadan_reflection,
-        eid_fitr
-    ]
-    return (
-        <div>
-        <h1 className='updates-title'>Information and Updates</h1>
-        <div className='updates-container snap' onScroll={()=>setSlideRight(false)}>
-            <div  className={slideRight? 'slide-right':'display-none '}  onClick={()=>setSlideRight(false)} >
+  const [mobileSlider, setMobileSlider] = useState(false);
+  //   const [slideRight, setSlideRight] = useState(true);
+  useEffect(() => {
+    if (window.innerWidth < 600) {
+      setMobileSlider(true);
+    } else {
+      setMobileSlider(false);
+    }
+    window.addEventListener("resize", () => {
+      if (window.innerWidth < 600) {
+        setMobileSlider(true);
+      } else {
+        setMobileSlider(false);
+      }
+    });
+  });
+  const images = [
+    eid_adha_2022,
+    arafat,
+    takbeer_tashreeq,
+    bengali_dates,
+    sacrifice_hijjah,
+    ramadan_reflection,
+    eid_fitr,
+  ];
+  return (
+    <div>
+      <h1 className="updates-title">Information and Updates</h1>
+      <div
+        className="updates-container snap"
+        // onScroll={() => setSlideRight(false)}
+      >
+        {/* <div  className={slideRight? 'slide-right':'display-none '}  onClick={()=>setSlideRight(false)} >
             Slide right for more
+            </div> */}
+        {images.map((img) => {
+          return (
+            <div className="updates-img">
+              <img
+                src={img}
+                alt=""
+                srcset=""
+                height={mobileSlider ? 350 : 550}
+              />
             </div>
-            {images.map((img)=>{
-                return(
-                    <div className='updates-img'>
-                        <img src={img} alt="" srcset="" height={mobileSlider ? 350:550}/>
-                    </div>
-                )
-            })}
-        </div>
+          );
+        })}
+      </div>
     </div>
-    )
+  );
 }
 
-export default Updates
+export default Updates;
